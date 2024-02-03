7fe2329475bd91488e7eb8a605652e1fd506374a
Keerthana M <145577874+keerthanam12@users.noreply.github.com>
2024-02-02 19:42:45 -0800

    Delete managementsystem -6/managementsystem directory

--- managementsystem -6/managementsystem/managementsystem/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ManagementsystemConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'managementsystem'
--- managementsystem -6/managementsystem/managementsystem/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import TestSubmission
-
-class TestSubmissionForm(forms.ModelForm):
-    class Meta:
-        model = TestSubmission
-        fields = ['answers']
